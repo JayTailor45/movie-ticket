8b4a041e3d9aac1a6cbcab74fb8ab56e72c3dc89
Jay Tailor <tailorj64@gmail.com>
2023-09-19 11:24:40 +0530

    Movies: App skaffold

---
+++ movies/README.md
@@ -0,0 +1,6 @@
+### Node express app
+
+A node application used to handle movies REST endpoints.
+
+API collection is attached at the root folder.
+
---
+++ movies/src/index.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import mongoose from "mongoose";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { middlewares as mw, errors as Err } from "@tj-movies-ticket/common";
+import { natsWrapper } from "./nats-wrapper";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(mw.currentUser);
+
+app.get("*", async () => {
+  throw new Err.NotFoundError();
+});
+
+app.use(mw.errorHandler);
+
+const start = async () => {
+  // check if env variables are defined
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY is not found");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URL is not found");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL is not found");
+  }
+  if (!process.env.NATS_CLUSTER) {
+    throw new Error("NATS_CLUSTER is not found");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID is not found");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER,
+      process.env.NATS_CLIENT_ID, // <-- Client id will be pod id/name
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Movies app connected to mongodb");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("Movies service listening on port", 3000);
+  });
+};
+
+start();
